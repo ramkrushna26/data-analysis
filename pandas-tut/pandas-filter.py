113bfc9376a0c93cb851c0dc54a800601b44efa8
ramkrushna26 <ramkrushna.bolewar1@gmail.com>
2023-12-17 13:56:10 +0530

    added files

--- pandas-tut/pandas-filter.py
+++ pandas-tut/pandas-filter.py
@@ -30,3 +30,75 @@ print("\nGet all batsman who scored Zero: \n",
 print("\nGet all batsman who scored between 100 or 200: \n", 
 	df[df["Runs"].between(150,200)][["Batsman_Name","Runs"]].head())
 
+print("\nno of record in dataframe: \n", df.index)
+df["Match_no"] = df["Match_no"].astype('category')
+df.info()
+
+df.set_index(keys="Match_no", inplace=True)
+print(df.head())
+df.reset_index(drop=False, inplace=True)
+print(df.head())
+
+print("\nIs 49 present in index: ", 49 in df.index)
+
+
+print("\nGet index rows from 2 to 5: \n", df.loc[2:5])
+print("\nGet index rows 50: \n", df.loc[50])
+
+
+print("\nGet rows where batsman is Rohit and runs scored is >=100: \n", 
+	df.loc[(df.Batsman_Name == "Rohit Sharma") & (df.Runs >= 100)])
+
+print("\n50 index match: \n", 
+	df.loc[50, "Match_Between"])
+
+print("\nGet names of batsman who didn't face any balls: \n", 
+	df.loc[(df.Balls == 0), ["Match_Between","Batsman_Name"]])
+
+
+print("\nrename field runs as score: \n", 
+	df.rename(mapper = {"Runs": "Score", "4s": "Fours", "6s": "Sixes"}, axis = 1).head())
+
+
+print("\ndrop column strike_rate: \n", 
+	df.drop(["Strike_Rate"], axis=1).head())
+
+# Remove column from dataframe
+#print(df.pop("Strike_Rate"))
+#print(df.head())
+
+print("\nSample: \n", df.sample())
+
+print("\nSort values by Runs in desc: \n", 
+	df.sort_values(by=["Runs"], ascending=False).head())
+
+print("\nTop 5 most runs scored : \n", 
+	df.nlargest(5, columns=["Runs"]))
+
+print("\nTop 5 least runs scored by Rohit: \n", 
+	df[df["Batsman_Name"] == "Rohit Sharma"].nsmallest(5, columns=["Runs"]))
+
+print("\nColumns in dataframe: \n", df.columns)
+
+print("\nBatsmans who hit more than 5 sixes: \n", 
+	df.where(df["6s"] > 5).dropna().head())
+
+print("\nRohit innings with more runs more than 50: \n", 
+	df.query("Batsman_Name == 'Rohit Sharma' and Runs > 50"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
